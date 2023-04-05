837e1212e07c6dc44f1acd7d3ceba9556dba3d62
Vasile Barbaros <vasile.barbaros@ebs-integrator.com>
2023-04-05 12:51:06 +0300

    log videoFile.getPath()

--- android/src/main/java/com/camera_deep_ar/CameraDeepArView.java
+++ android/src/main/java/com/camera_deep_ar/CameraDeepArView.java
@@ -248,6 +248,7 @@ public class CameraDeepArView implements PlatformView,
         } else if ("startVideoRecording".equals(methodCall.method)) {
             CharSequence now = DateFormat.format("yyyy_MM_dd_hh_mm_ss", new Date());
             videoFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES) + "/DeepAR_" + now + ".mp4");
+            Log.d("videoFile",videoFile.getPath());
             deepAR.startVideoRecording(videoFile.getPath());
             result.success("Video Recording Started");
         } else if ("stopVideoRecording".equals(methodCall.method)) {
